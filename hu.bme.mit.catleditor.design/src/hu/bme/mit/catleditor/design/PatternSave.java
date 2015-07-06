1634ac96bed8e7a6a5631a79dcc08726d7738fd5
György Bártfay <gbartfay@gmail.com>
2015-07-06 15:36:27 +0200

    Pattern Save & Load menu added

---
+++ hu.bme.mit.catleditor.design/src/hu/bme/mit/catleditor/design/PatternSave.java
@@ -0,0 +1,27 @@
+package hu.bme.mit.catleditor.design;
+
+import java.util.Collection;
+import java.util.Map;
+
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.sirius.tools.api.ui.IExternalJavaAction;
+
+public class PatternSave implements IExternalJavaAction {
+
+	public PatternSave() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void execute(Collection<? extends EObject> selections, Map<String, Object> parameters) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean canExecute(Collection<? extends EObject> selections) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
